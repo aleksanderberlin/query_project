dcfc0191bd924cc26592b32686974c5391e8769c
aleksandrberlin <aleksanderberlin@gmail.com>
2020-11-10 00:14:46 +0300

    add messages on server settings

--- server/views.py
+++ server/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import permission_required
 # Create your views here.
 from .forms import *
 from constance import config
+from django.contrib import messages
 
 
 @permission_required('database.change_server_settings')
@@ -14,6 +15,9 @@ def server_settings(request):
             config.EXCLUDE_WEEKDAYS = settings_form.cleaned_data['exclude_weekdays']
             config.TIME_OPENING = settings_form.cleaned_data['time_opening']
             config.TIME_CLOSING = settings_form.cleaned_data['time_closing']
+            messages.SUCCESS('Изменения успешно сохранены')
+        else:
+            messages.ERROR('Введенные параметры некорректны.')
     settings_form = ServerSettingsForm(initial={'exclude_weekdays': config.EXCLUDE_WEEKDAYS,
                                                 'time_opening': config.TIME_OPENING,
                                                 'time_closing': config.TIME_CLOSING})

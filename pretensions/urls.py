ad2c8452c89f968daf1f03f0a2e5e77f897c96c4
aleksandrberlin <aleksanderberlin@gmail.com>
2020-12-07 22:10:45 +0300

    add pretensions module

--- pretensions/urls.py
+++ pretensions/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from pretensions.views import PretensionsFormView
 
 urlpatterns = [
-    # path('', PretensionsFormView.as_view(), name='pretension_form'),
+    path('', PretensionsFormView.as_view(), name='pretension_form'),
 ]
